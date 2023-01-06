6e1a5f8deae767b733a915d1b4e4bdf4488dfd7d
Ronald <ron556611@gmail.com>
2023-01-06 17:23:08 +0800

    s

--- src/server/trpc/router/room.ts
+++ src/server/trpc/router/room.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import { User } from "../../../types/User";
 import { isLoggedIn, login, logout } from "../../../utils/session";
 import { router, publicProcedure, protectedProcedure } from "../trpc";
+import { MeetingRoomAttendeeDatetimeRangeDatetimeMode } from "@prisma/client";
 
 export const roomRouter = router({
     getRoomBySession: protectedProcedure
@@ -193,5 +194,111 @@ export const roomRouter = router({
             return {
                 result: result,
             };
+        }),
+    submitMeetingTime: protectedProcedure
+        .input(
+            z.object({
+                startDateTime: z.date(),
+                endDateTime: z.date(),
+                datetimeMode : z.nativeEnum(MeetingRoomAttendeeDatetimeRangeDatetimeMode),
+            })
+        )
+        .mutation(async ({ input, ctx }) => {
+            const user = ctx.request.req.session.user;
+
+            const attendee = await ctx.prisma.meetingRoomAttendee.findUnique({
+                where: {
+                    id: user!.meetingRoomAttendeeId,
+                },
+            });
+
+            if (!attendee) {
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                });
+            }
+            
+            //check if the startDateTime >= input.startDateTime and startDateTime <= input.startDateTime OR endDateTime >= input.startDateTime and endDateTime <= input.endDateTime
+            const existingAttendeeDatetimeRange = await ctx.prisma.meetingRoomAttendeeDatetimeRange.findMany({
+                where: {
+                    meetingRoomAttendeeId: attendee.id,
+                    OR: [
+                        {
+                            startDateTime: {
+                                gte: input.startDateTime,
+                                lte: input.endDateTime,
+                            },
+                            endDateTime: {
+                                gte: input.startDateTime,
+                                lte: input.endDateTime,
+                            },
+                        }
+                    ]
+                },
+            });
+
+            if (existingAttendeeDatetimeRange.length > 0) {
+                throw new TRPCError({
+                    code: "BAD_REQUEST",
+                    message: "You have overlapping time slots",
+                });
+            }
+
+            const attendeeDatetimeRange = await ctx.prisma.meetingRoomAttendeeDatetimeRange.create(
+                {
+                    data: {
+                        meetingRoomAttendeeId: attendee.id,
+                        startDateTime: input.startDateTime,
+                        endDateTime: input.endDateTime,
+                        datetimeMode : input.datetimeMode,
+                    },
+                }
+            );
+
+            return attendeeDatetimeRange;
+        }),
+    confirmMeetingByHost : protectedProcedure
+        .input(
+            z.object({
+                startTime: z.date(),
+                endTime: z.date(),
+            })
+        )
+        .mutation(async ({ input, ctx }) => {
+            const user = ctx.request.req.session.user;
+            
+            const attendee = await ctx.prisma.meetingRoomAttendee.findUnique({
+                where: {
+                    id: user!.meetingRoomAttendeeId,
+                },
+            });
+
+            if (!attendee || !attendee.isHost) {
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                });
+            }
+
+            //update the room
+            const room = await ctx.prisma.meetingRoom.update({
+                where: {
+                    id: user!.meetingRoomId,
+                },
+                data: {
+                    actualStartTime: input.startTime,
+                    actualEndTime: input.endTime,
+                },
+            });
+
+            if (!room) {
+                throw new TRPCError({
+                    code: "NOT_FOUND",
+                });
+            }
+
+            return {
+                result: true,
+            };
+
         }),
 });
